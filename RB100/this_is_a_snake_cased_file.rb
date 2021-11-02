bdd79f92acafcaf6e71b62ab5dffaf21933bf92c
Miltos Stella <miltos.stella@gmail.com>
2021-11-01 19:18:03 -0700

    commit of exercises done

---
+++ RB100/this_is_a_snake_cased_file.rb
@@ -0,0 +1,26 @@
+# assigning a variable
+forty_two = 42
+
+# defining a method
+def this_is_a_great_method
+  # do stuff
+end
+
+# constant declaration
+FOUR = 'four'
+FIVE = 5
+
+# multi-line
+[1, 2, 3].each do |i|
+  # do stuff
+end
+
+# does the same thing in a single line
+[1, 2, 3].each { |i| do_some_stuff }
+
+# class naming
+class MyFirstClass
+end
+
+class MySecondClass
+end
